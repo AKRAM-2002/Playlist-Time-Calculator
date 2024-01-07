d42f76997da85707531f0ec69362b31d041da08a
akram <akramboutzouga@gmail.com>
2024-01-07 22:21:37 +0100

    setting up the project

---
+++ background.js
@@ -0,0 +1,2 @@
+// Background script for handling calculations and storing data
+// Use chrome extension APIs for storage and communication
---
+++ content.js
@@ -0,0 +1,3 @@
+// Content script to run on youtube pages
+// Extract playlist information from the current playlist
+// and send it to the background script
